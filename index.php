fa6ac763b6316e9eae817fb074244e75249a1507
brightonmike <brightonmike@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-07 16:12:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/coffee-admin-theme/trunk@515906 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ Author URI: http://michaelgunner.co.uk
 
 function load_custom_wp_admin_style(){
 $options = get_option('dashtheme_sample');
-        wp_register_style( 'custom_wp_admin_css', plugin_dir_url(__FILE__) . $options['option1']. $options['themeselect'] . '', false, '1.0.0' );
+        wp_register_style( 'custom_wp_admin_css', plugin_dir_url(__FILE__) . $options['option1']. $options['themeselect'] . '.css', false, '1.0.0' );
         wp_enqueue_style( 'custom_wp_admin_css' );
 }
 add_action('admin_enqueue_scripts', 'load_custom_wp_admin_style');
